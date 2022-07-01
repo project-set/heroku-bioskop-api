c19c0238c9e8a949430ab414c48e8fd3d70cf150
bayuaswin <bayuaswin@gmail.com>
2022-07-01 10:51:30 +0700

    add seats

--- src/main/java/com/infosys/timd/bioskopapi/Repository/SeatsRepository.java
+++ src/main/java/com/infosys/timd/bioskopapi/Repository/SeatsRepository.java
@@ -1,10 +1,6 @@
 package com.infosys.timd.bioskopapi.Repository;
 
-import com.infosys.timd.bioskopapi.DTO.*;
 import com.infosys.timd.bioskopapi.Model.*;
-import com.infosys.timd.bioskopapi.Exception.*;
-import com.infosys.timd.bioskopapi.Response.*;
-import com.infosys.timd.bioskopapi.Service.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
